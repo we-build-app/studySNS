09d920acc9b222d14605bd331c89a50a0ceeecd3
MoonInbae <mib0213@naver.com>
2021-04-05 23:21:41 +0900

    First commit

---
+++ Setting/app/src/main/java/com/example/setting/SettingPassword.java
@@ -0,0 +1,16 @@
+package com.example.setting;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SettingPassword extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_setting_password, container, false);
+    }
+}
---
+++ Setting/app/src/main/java/com/example/setting/SettingsFragment.java
@@ -0,0 +1,87 @@
+package com.example.setting;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.navigation.Navigation;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    private Preference password, email, nickname, profile_image, rules, dark_mode, alarm_setting, app_version,
+            FAQ, notice, user_agreement, personal_information_handling_policy, licence, information_using_agreement, withdrawal, logout;
+
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+
+    }
+
+//    on
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        this.password = (Preference) findPreference("password");
+        this.email = (Preference) findPreference("email");
+        this.nickname = (Preference) findPreference("nickname");
+        this.profile_image = (Preference) findPreference("profile_image");
+        this.rules = (Preference) findPreference("rules");
+        this.dark_mode = (Preference) findPreference("dark_mode");
+        this.alarm_setting = (Preference) findPreference("alarm_setting");
+        this.app_version = (Preference) findPreference("app_version");
+        this.FAQ = (Preference) findPreference("FAQ");
+        this.notice = (Preference) findPreference("notice");
+        this.user_agreement = (Preference) findPreference("user_agreement");
+        this.personal_information_handling_policy = (Preference) findPreference("personal_information_handling_policy");
+        this.licence = (Preference) findPreference("licence");
+        this.information_using_agreement = (Preference) findPreference("information_using_agreement");
+        this.withdrawal = (Preference) findPreference("withdrawal");
+        this.logout = (Preference) findPreference("logout");
+
+
+        this.password.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Navigation.findNavController(view).navigate(R.id.action_settingsFragment_to_settingPassword);
+                return true;
+            }
+        });
+        this.email.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Navigation.findNavController(view).navigate(R.id.action_settingsFragment_to_settingEmail);
+                return true;
+            }
+        });
+        this.nickname.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Navigation.findNavController(view).navigate(R.id.action_settingsFragment_to_settingNickname);
+                return true;
+            }
+        });
+        this.rules.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Navigation.findNavController(view).navigate(R.id.action_settingsFragment_to_settingRule);
+                return true;
+            }
+        });
+    }
+
+    //    @Override
+//    public boolean onPreferenceTreeClick(androidx.preference.Preference preference) {
+//        if(preference.getKey().equals("nickname")){
+////            setPreferencesFromResource(R.layout.fragment_setting_nickname, rootKey);
+//        }
+//        return super.onPreferenceTreeClick(preference);
+//    }
+}
