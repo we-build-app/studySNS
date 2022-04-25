701198c1b32878f923301623ab344c4a51f83a24
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2022-04-26 03:36:29 +0900

    커뮤니티 - 탭 레이아웃
    커뮤니티 탭 레이아웃 만들기

--- StudySNS/app/src/main/java/com/example/domain/view/home/HomeFragment.java
+++ StudySNS/app/src/main/java/com/example/domain/view/home/HomeFragment.java
@@ -10,7 +10,7 @@ public class HomeFragment extends AbstractFragment {
 
     // Associate
         // View
-        private Button loginButton, settingButton;
+        private Button loginButton, settingButton, MenuButton;
 
     @Override protected int getViewId() { return R.layout.fragment_home; }
 
@@ -18,11 +18,13 @@ public class HomeFragment extends AbstractFragment {
     protected void associateView(View view) {
         this.loginButton = view.findViewById(R.id.loginButton);
         this.settingButton = view.findViewById(R.id.settingButton);
+        this.MenuButton = view.findViewById(R.id.menu1);
     }
 
     @Override
     protected void initialize() {
         this.loginButton.setOnClickListener(v -> this.navigateTo(R.id.action_homeFragment_to_loginFragment));
         this.settingButton.setOnClickListener(v -> this.navigateTo(R.id.action_homeFragment_to_settingsFragment));
+        this.MenuButton.setOnClickListener(v -> this.navigateTo(R.id.action_homeFragment_to_home_Communtiy));
     }
 }
---
+++ StudySNS/app/src/main/java/com/example/domain/view/home/setting/HomeCommuntiy.java
@@ -0,0 +1,69 @@
+package com.example.domain.view.home.setting;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Switch;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class HomeCommuntiy extends Fragment {
+    private TabLayout tabs;
+
+    //탭 레이아웃
+    private HomeCommnutiyAllBoard AllBoard;
+    private HomeCommuntiyPopularBoard PopularBoard;
+    private HomeCommunityStudyOpenings StudyOpenings;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home_communtiy, container, false);
+
+        this.AllBoard = new HomeCommnutiyAllBoard();
+        this.PopularBoard = new HomeCommuntiyPopularBoard();
+        this.StudyOpenings = new HomeCommunityStudyOpenings();
+
+        this.tabs = view.findViewById(R.id.hometabs);
+
+        //탭 레이아웃 설정부분 - 탭 레이아웃 클릭시 Fragment를 바꾸는 코드
+        getActivity().getSupportFragmentManager().beginTransaction().add(R.id.homeMain, this.AllBoard).commit();
+
+        this.tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                Fragment selected = null;
+                switch (position){
+                    case 0:
+                        AllBoard = new HomeCommnutiyAllBoard();
+                        selected = AllBoard;
+                        break;
+                    case 1:
+                        PopularBoard = new HomeCommuntiyPopularBoard();
+                        selected = PopularBoard;
+                        break;
+                    case 2:
+                        StudyOpenings = new HomeCommunityStudyOpenings();
+                        selected = StudyOpenings;
+                        break;
+                }
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.homeMain, selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+
+        return view;
+    }
+}
