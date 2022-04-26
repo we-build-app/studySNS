e476a5df3992661c2e4ae993b537c759e8e283d4
MoonInbae <mib010213@gmail.com>
2022-04-26 16:43:11 +0900

    MainActivity(UI) 완성
    MainActivity(UI) 완성

--- StudySNS/app/src/main/java/com/example/domain/view/MainActivity.java
+++ StudySNS/app/src/main/java/com/example/domain/view/MainActivity.java
@@ -1,30 +1,92 @@
 package com.example.domain.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.navigation.Navigation;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.Space;
 
 import com.example.domain.view.home.setting.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Space status_bar_space;
+
+    private int statusBarHeight;
+
+    private BottomNavigationView bottomNavigationView;
+
+    private FragmentContainerView main_fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar tb = (Toolbar) findViewById(R.id.toolbar) ;
-        tb.setOnClickListener();
+//        Toolbar tb = (Toolbar) findViewById(R.id.toolbar) ;
+//        tb.setBackgroundColor(Color.WHITE);
+//        tb.setTitleTextColor(Color.BLACK);
+
+//        tb.setOnClickListener();
 //        setSupportActionBar(tb) ;
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+        this.statusBarHeight = 0;
+        int resId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if(resId>0){
+            this.statusBarHeight = getResources().getDimensionPixelSize(resId);
+        }
+
+        this.status_bar_space = findViewById(R.id.status_bar_space);
+
+        this.status_bar_space.getLayoutParams().height = statusBarHeight;
+
+        this.bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        this.main_fragment = findViewById(R.id.main_fragment);
+
+        this.bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.homeBTN:
+                        Navigation.findNavController(main_fragment).navigate(R.id.action_homeFragment_to_loginFragment);
+                        break;
+                    case R.id.communityBTN:
+//                        Navigation.findNavController(main_fragment).navigate(R.id.action_homeFragment_to_settingsFragment);
+                        break;
+                    case R.id.plannerBTN:
+//                        Navigation.findNavController(main_fragment).navigate(R.id.action_homeFragment_to_loginFragment);
+                        break;
+                    case R.id.settingBTN:
+                        Navigation.findNavController(main_fragment).navigate(R.id.action_homeFragment_to_settingsFragment);
+                        break;
+                }
+                return false;
+            }
+        });
+
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_option);
+        inflater.inflate(R.menu.menu_option, menu);
+
         return super.onCreateOptionsMenu(menu);
     }
 }
