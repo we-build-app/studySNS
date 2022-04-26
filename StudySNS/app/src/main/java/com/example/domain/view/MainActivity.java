428ad5d8337990db36eb13de471fd13c71e7ad8f
MoonInbae <mib010213@gmail.com>
2022-04-26 14:08:28 +0900

    toolbar 작업중
    toolbar 작업중

--- StudySNS/app/src/main/java/com/example/domain/view/MainActivity.java
+++ StudySNS/app/src/main/java/com/example/domain/view/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.domain.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.example.domain.view.home.setting.R;
 
@@ -12,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar tb = (Toolbar) findViewById(R.id.toolbar) ;
+        tb.setOnClickListener();
+//        setSupportActionBar(tb) ;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_option);
+        return super.onCreateOptionsMenu(menu);
     }
 }
