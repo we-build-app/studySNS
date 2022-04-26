3d083c95beb66c2bbb43a8c21c0a84ce91ec93f0
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2022-04-27 00:54:46 +0900

    Login - Password 텍스트 뷰 연결
    1. 회원가입, 아이디 찾기 부분 연결
    2. 찾기 클릭시 비밀번호 찾기 결과 창 연결

--- StudySNS/app/src/main/java/com/example/domain/view/home/login/LoginPWFindFragment.java
+++ StudySNS/app/src/main/java/com/example/domain/view/home/login/LoginPWFindFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
@@ -16,6 +17,7 @@ public class LoginPWFindFragment extends Fragment {
     // Associate
         // View
         private Button pwFindButton;
+        private TextView IDFindText, RegisterText;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -28,8 +30,12 @@ public class LoginPWFindFragment extends Fragment {
 
         // Associate View
         this.pwFindButton = view.findViewById(R.id.pwFindButton);
+        this.IDFindText = view.findViewById(R.id.FindPW_IDText);
+        this.RegisterText = view.findViewById(R.id.FindPW_RegisterText);
 
         // Set View Callback
-        this.pwFindButton.setOnClickListener(v-> Navigation.findNavController(view).navigate(R.id.action_global_homeFragment));
+        this.pwFindButton.setOnClickListener(v-> Navigation.findNavController(view).navigate(R.id.action_loginPWFind_to_loginPWFindResult));
+        this.IDFindText.setOnClickListener(v-> Navigation.findNavController(view).navigate(R.id.loginIDFind));
+        this.RegisterText.setOnClickListener(v-> Navigation.findNavController(view).navigate(R.id.loginAccountCreate));
     }
 }
