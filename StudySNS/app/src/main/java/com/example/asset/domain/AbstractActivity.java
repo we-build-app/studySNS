2d11b164967985cf2e5a93c2c2818c92e15cb29e
MoonInbae <mib010213@gmail.com>
2022-04-26 17:47:25 +0900

    Update AbstractActivity.java

--- StudySNS/app/src/main/java/com/example/asset/domain/AbstractActivity.java
+++ StudySNS/app/src/main/java/com/example/asset/domain/AbstractActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public abstract class AbstractActicity extends AppCompatActivity {
+public abstract class AbstractActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
