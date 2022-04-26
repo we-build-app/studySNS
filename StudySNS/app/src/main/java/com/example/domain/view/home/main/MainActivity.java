21e0582eb983912efbec0477e42f73dd41be665a
MoonInbae <mib010213@gmail.com>
2022-04-26 21:49:22 +0900

    AbstractActivity 완성
    AbstractActivity 완성

--- StudySNS/app/src/main/java/com/example/domain/view/home/main/MainActivity.java
+++ StudySNS/app/src/main/java/com/example/domain/view/home/main/MainActivity.java
@@ -14,79 +14,55 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Space;
 
+import com.example.asset.domain.AbstractActivity;
 import com.example.domain.view.home.setting.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AbstractActivity {
 
     private Space status_bar_space;
-
-    private int statusBarHeight;
 
     private BottomNavigationView bottomNavigationView;
 
     private FragmentContainerView main_fragment;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getViewId() { return R.layout.activity_main; }
 
-//        Toolbar tb = (Toolbar) findViewById(R.id.toolbar) ;
-//        tb.setBackgroundColor(Color.WHITE);
-//        tb.setTitleTextColor(Color.BLACK);
-
-//        tb.setOnClickListener();
-//        setSupportActionBar(tb) ;
-
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
-
-        this.statusBarHeight = 0;
-        int resId = getResources().getIdentifier("status_bar_height", "dimen", "android");
-        if(resId>0){
-            this.statusBarHeight = getResources().getDimensionPixelSize(resId);
-        }
-
+    @Override
+    protected void associateView() {
         this.status_bar_space = findViewById(R.id.status_bar_space);
-
-        this.status_bar_space.getLayoutParams().height = statusBarHeight;
-
         this.bottomNavigationView = findViewById(R.id.bottomNavigationView);
-
         this.main_fragment = findViewById(R.id.main_fragment);
+    }
 
-        this.bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+    @Override
+    protected void initialize() {
+        this.controlTopSpace(this.status_bar_space);
+        this.bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.homeBTN:
-                        Navigation.findNavController(main_fragment).navigate(R.id.homeFragment);
+                        navigateTo(main_fragment, R.id.homeFragment);
                         break;
                     case R.id.communityBTN:
-                        Navigation.findNavController(main_fragment).navigate(R.id.home_Communtiy);
+                        navigateTo(main_fragment, R.id.home_Communtiy);
                         break;
                     case R.id.plannerBTN:
-//                        Navigation.findNavController(main_fragment).navigate(R.id.action_homeFragment_to_loginFragment);
+//                        navigateTo(main_fragment, R.id.homeFragment);
                         break;
                     case R.id.settingBTN:
-                        Navigation.findNavController(main_fragment).navigate(R.id.settingsFragment);
+                        navigateTo(main_fragment, R.id.settingsFragment);
                         break;
                 }
-                return false;
+                return true;
             }
         });
-
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_option, menu);
-
-        return super.onCreateOptionsMenu(menu);
     }
 }
