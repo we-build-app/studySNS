84f72a99c8b63010c8fc9467f5bf86c309371025
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2022-04-27 10:00:46 +0900

    Register - 작업중
    레지스터 부분 회원가입 작업중

--- StudySNS/app/src/main/java/com/example/domain/view/home/login/LoginAccountCreateFragment.java
+++ StudySNS/app/src/main/java/com/example/domain/view/home/login/LoginAccountCreateFragment.java
@@ -1,10 +1,14 @@
 package com.example.domain.view.home.login;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
@@ -12,10 +16,14 @@ import androidx.navigation.Navigation;
 import com.example.domain.view.home.setting.R;
 
 public class LoginAccountCreateFragment extends Fragment {
-
     // Associate
         // View
         private Button signInButton;
+        private EditText name, userID, password, Repassword, Email, college;
+
+        // System
+        private InputMethodManager inputMethodManager;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -26,10 +34,49 @@ public class LoginAccountCreateFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        this.inputMethodManager = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
         // Associate View
         this.signInButton = view.findViewById(R.id.signInButton);
+        this.name = view.findViewById(R.id.editName);
+        this.userID = view.findViewById(R.id.editId);
+        this.password = view.findViewById(R.id.editPw);
+        this.Repassword = view.findViewById(R.id.editPwRe);
+        this.Email = view.findViewById(R.id.editEmail);
+        this.college = view.findViewById(R.id.editUniv);
+
+        //회원가입시 공백이 있는지 체크
+        verfiyBlankIntheRegisterSection();
 
         // Set View Callback
-        this.signInButton.setOnClickListener(v-> Navigation.findNavController(view).navigate(R.id.action_global_homeFragment));
+//        this.signInButton.setOnClickListener(v-> Navigation.findNavController(view).navigate(R.id.action_global_homeFragment));
+    }
+
+    private void verfiyBlankIntheRegisterSection() {
+        this.signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(name.getText().toString().equals("")){
+                    Toast.makeText(getContext(),"이름를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                    name.requestFocus();
+                }
+                else if(userID.getText().toString().equals("")){
+                    Toast.makeText(getContext(),"아이디를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                    userID.requestFocus();
+                } else if(password.getText().toString().equals("")){
+                    Toast.makeText(getContext(),"비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                    password.requestFocus();
+                } else if(userID.getText().toString().equals("")){
+                    Toast.makeText(getContext(),"아이디를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                    userID.requestFocus();
+                } else if(Email.getText().toString().equals("")){
+                    Toast.makeText(getContext(),"이메일을 입력해주세요.", Toast.LENGTH_SHORT).show();
+                    Email.requestFocus();
+                } else if(college.getText().toString().equals("")){
+                    Toast.makeText(getContext(),"대학교를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                    college.requestFocus();
+                }
+                inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+            }
+        });
     }
 }
