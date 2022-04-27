c08a949bcd56275215d15babc3f74948e7278b41
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2022-04-28 02:16:23 +0900

    Register - 닉네임 설정
    1. 닉네임 레이아웃 및 디자인 완료
    2. 닉네임 중복확인 작업해야함

--- StudySNS/app/src/main/java/com/example/domain/view/home/login/LoginAccountCreateAgree2.java
+++ StudySNS/app/src/main/java/com/example/domain/view/home/login/LoginAccountCreateAgree2.java
@@ -1,4 +1,4 @@
-package com.example.domain.view.home.setting;
+package com.example.domain.view.home.login;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.domain.view.home.setting.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link LoginAccountCreateAgree1#newInstance} factory method to
+ * Use the {@link LoginAccountCreateAgree2#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LoginAccountCreateAgree1 extends Fragment {
+public class LoginAccountCreateAgree2 extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class LoginAccountCreateAgree1 extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public LoginAccountCreateAgree1() {
+    public LoginAccountCreateAgree2() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class LoginAccountCreateAgree1 extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginAccountCreateAgree1.
+     * @return A new instance of fragment LoginAccountCreateAgree2.
      */
     // TODO: Rename and change types and number of parameters
-    public static LoginAccountCreateAgree1 newInstance(String param1, String param2) {
-        LoginAccountCreateAgree1 fragment = new LoginAccountCreateAgree1();
+    public static LoginAccountCreateAgree2 newInstance(String param1, String param2) {
+        LoginAccountCreateAgree2 fragment = new LoginAccountCreateAgree2();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class LoginAccountCreateAgree1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_account_create_agree1, container, false);
+        return inflater.inflate(R.layout.fragment_login_account_create_agree2, container, false);
     }
 }
--- StudySNS/app/src/main/java/com/example/domain/view/home/login/LoginAccountCreateSetNickName.java
+++ StudySNS/app/src/main/java/com/example/domain/view/home/login/LoginAccountCreateSetNickName.java
@@ -3,64 +3,64 @@ package com.example.domain.view.home.login;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.domain.view.home.setting.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LoginAccountCreateSetNickName#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class LoginAccountCreateSetNickName extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class LoginAccountCreateSetNickName extends Fragment  {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public LoginAccountCreateSetNickName() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginAccountCreateSetNickName.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LoginAccountCreateSetNickName newInstance(String param1, String param2) {
-        LoginAccountCreateSetNickName fragment = new LoginAccountCreateSetNickName();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private EditText NickName;
+    private TextView TextSize;
+    private Button CompleteBTN;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_account_create_set_nick_name, container, false);
+        View view = inflater.inflate(R.layout.fragment_login_account_create_set_nick_name, container, false);
+
+        this.NickName = view.findViewById(R.id.nickinput);
+        this.TextSize = view.findViewById(R.id.textsize);
+        this.CompleteBTN = view.findViewById(R.id.RegisterComplete);
+
+        NickName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String input = NickName.getText().toString();
+                TextSize.setText(String.valueOf(input.length())+" / 10");
+            }
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+
+        this.CompleteBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(NickName.getText().toString().equals("")){
+                    Toast.makeText(getContext(), "닉네임을 입력해주세요.", Toast.LENGTH_SHORT).show();
+                }
+//                else if(){
+//                  //중복확인 체크 부분
+//                }
+                else{
+                    Navigation.findNavController(view).navigate(R.id.loginFragment);
+                }
+            }
+        });
+
+        return view;
     }
 }
