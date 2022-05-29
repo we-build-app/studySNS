04402489d93b590e0636a59648be316fe89366f3
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2022-05-29 22:19:20 +0900

    0.1-CKS [FEAT] 비밀번호 찾기 뒤로가기 버튼 이벤트, 추가
    - 비밀번호 찾기에서 로그인 화면으로 가는 뒤로가기 버튼 이벤트 추가

--- StudySNS/app/src/main/java/com/example/domain/view/home/view/login/LoginPWFindFragment.java
+++ StudySNS/app/src/main/java/com/example/domain/view/home/view/login/LoginPWFindFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -16,8 +17,9 @@ public class LoginPWFindFragment extends Fragment {
 
     // Associate
         // View
-        private Button pwFindButton;
-        private TextView IDFindText, RegisterText;
+        private Button pw_find_btn;
+        private TextView pw_id_text, pw_register_text;
+        private ImageView pw_back_btn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -29,13 +31,15 @@ public class LoginPWFindFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         // Associate View
-        this.pwFindButton = view.findViewById(R.id.pwFindButton);
-        this.IDFindText = view.findViewById(R.id.FindPW_IDText);
-        this.RegisterText = view.findViewById(R.id.FindPW_RegisterText);
+        this.pw_find_btn = view.findViewById(R.id.pwFindButton);
+        this.pw_id_text = view.findViewById(R.id.FindPW_IDText);
+        this.pw_register_text = view.findViewById(R.id.FindPW_RegisterText);
+        this.pw_back_btn = view.findViewById(R.id.find_password_back_btn);
 
         // Set View Callback
-        this.pwFindButton.setOnClickListener(v-> Navigation.findNavController(view).navigate(R.id.action_loginPWFind_to_loginPWFindResult));
-        this.IDFindText.setOnClickListener(v-> Navigation.findNavController(view).navigate(R.id.loginIDFind));
-        this.RegisterText.setOnClickListener(v-> Navigation.findNavController(view).navigate(R.id.loginAccountCreate));
+        this.pw_find_btn.setOnClickListener(v-> Navigation.findNavController(view).navigate(R.id.action_loginPWFind_to_loginPWFindResult));
+        this.pw_id_text.setOnClickListener(v-> Navigation.findNavController(view).navigate(R.id.loginIDFind));
+        this.pw_register_text.setOnClickListener(v-> Navigation.findNavController(view).navigate(R.id.loginAccountCreate));
+        this.pw_back_btn.setOnClickListener(v -> Navigation.findNavController(view).navigate(R.id.loginFragment));
     }
 }
