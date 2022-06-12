a71c863ef8508bea8b8809ac1c4548312ce60860
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2022-06-12 09:10:13 +0900

    0.2-CKS[REFACTOR] findViewbyID -> View binding으로 변환
    - LoginPWFindFragment를 findViewbyId를 사용하는 것을 ViewBinding으로 리팩토링 하여 코드 단순화

--- StudySNS/app/src/main/java/com/softcon/studysns/view/home/view/login/LoginPWFindFragment.java
+++ StudySNS/app/src/main/java/com/softcon/studysns/view/home/view/login/LoginPWFindFragment.java
@@ -12,34 +12,27 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 import com.softcon.studysns.view.home.setting.R;
+import com.softcon.studysns.view.home.setting.databinding.FragmentLoginPwFindBinding;
 
 public class LoginPWFindFragment extends Fragment {
 
     // Associate
         // View
-        private Button pw_find_btn;
-        private TextView pw_id_text, pw_register_text;
-        private ImageView pw_back_btn;
+        public FragmentLoginPwFindBinding pw_binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login_pw_find, container, false);
+        pw_binding = FragmentLoginPwFindBinding.inflate(inflater, container, false);
+        return pw_binding.getRoot();
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        // Associate View
-        this.pw_find_btn = view.findViewById(R.id.pwFindButton);
-        this.pw_id_text = view.findViewById(R.id.FindPW_IDText);
-        this.pw_register_text = view.findViewById(R.id.FindPW_RegisterText);
-        this.pw_back_btn = view.findViewById(R.id.find_password_back_btn);
-
         // Set View Callback
-        this.pw_find_btn.setOnClickListener(v-> Navigation.findNavController(view).navigate(R.id.action_loginPWFind_to_loginPWFindResult));
-        this.pw_id_text.setOnClickListener(v-> Navigation.findNavController(view).navigate(R.id.loginIDFind));
-        this.pw_register_text.setOnClickListener(v-> Navigation.findNavController(view).navigate(R.id.loginAccountCreate));
-        this.pw_back_btn.setOnClickListener(v -> Navigation.findNavController(view).navigate(R.id.loginFragment));
+        pw_binding.pwFindButton.setOnClickListener(v-> Navigation.findNavController(view).navigate(R.id.action_loginPWFind_to_loginPWFindResult));
+        pw_binding.FindPWIDText.setOnClickListener(v-> Navigation.findNavController(view).navigate(R.id.loginIDFind));
+        pw_binding.FindPWRegisterText.setOnClickListener(v-> Navigation.findNavController(view).navigate(R.id.loginAccountCreate));
+        pw_binding.findPasswordBackBtn.setOnClickListener(v -> Navigation.findNavController(view).navigate(R.id.loginFragment));
     }
 }
